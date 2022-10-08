ff58eb1347bdfcc0c50968f160e7f279f2bf6ec0
JS <leejiseonghope20@gmail.com>
2022-10-08 21:36:21 +0900

    second commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // 앱에서 표시할 url 입력
+        webView.loadUrl("https://www.bokjiro.go.kr/ssis-teu/index.do");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    //폰의 뒤로가기 버튼의 동작 입력
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
